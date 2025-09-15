3383b8c0c57c672fa54f41f6f43d6c71a63342b3
Simon-Pruvot <simon.pruvot@ynov.com>
2025-09-15 11:27:29 +0200

    h

---
+++ src/poisonpot.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func (c *Character) UsePoison() {
+	for i := 0; i < len(c.inv); i++ {
+		if c.inv[i].nom == "Potion de poison" && c.inv[i].quantity > 0 {
+			c.inv[i].quantity--
+			c.Hp -= 10
+			time.Sleep(1 * time.Second)
+			c.Hp -= 10
+			time.Sleep(1 * time.Second)
+			c.Hp -= 10
+			if c.Hp > c.HpMax {
+				c.Hp = c.HpMax
+			}
+			fmt.Println("You make damage to your enemi")
+			fmt.Println(``)
+
+			// if no more left → remove it
+			if c.inv[i].quantity == 0 {
+				c.inv = append(c.inv[:i], c.inv[i+1:]...)
+			}
+			return
+		}
+	}
+	fmt.Println("⚠️ No potion of poison left!")
+}
