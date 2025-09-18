6573b44d879b4f6d3f9fec1e258a6ca524914fc5
Bura-Kevin <kevin.bura@ynov.com>
2025-09-18 10:16:54 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -19,24 +19,6 @@ func main() {
 	armorLegend := Objects{nom: "Armor A", quantity: 1}
 	potionLegend := Objects{nom: "Potion Rage", quantity: 1}
 
-	var objects = map[string]Objects{
-		"Armor A": {nom: "Armor A", quantity: 1},
-		"Armor B": {nom: "Armor B", quantity: 1},
-		"Armor C": {nom: "Armor C", quantity: 1},
-
-		"Chapeau A": {nom: "Chapeau A", quantity: 1},
-		"Tunique A": {nom: "Tunique A", quantity: 1},
-		"Bottes A":  {nom: "Bottes A", quantity: 1},
-
-		"Chapeau B": {nom: "Chapeau B", quantity: 1},
-		"Tunique B": {nom: "Tunique B", quantity: 1},
-		"Bottes B":  {nom: "Bottes B", quantity: 1},
-
-		"Chapeau C": {nom: "Chapeau C", quantity: 1},
-		"Tunique C": {nom: "Tunique C", quantity: 1},
-		"Bottes C":  {nom: "Bottes C", quantity: 1},
-	}
-
 	//J'appelle la fonction de l'écran de départ
 	chosendif := TextBienvenu()
 
